b61c2f363ac5c0be4c98ed3ff533eb6d214308e0
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-06-06 07:08:02 -0400

    Break

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Portfolio from "./Portfolio";
+import AboutSection from "./AboutSection";
+import ContactSection from "./ContactSection"
 import Footer from "./Footer";
+import Copyright from "./Copyright";
 
 const App = () => (
     <div>
         <Navbar />
         <Header />
         <Portfolio />
+        <AboutSection />
+        <ContactSection />
         <Footer />
+        <Copyright />
     </div>
 )
 
--- src/Footer.js
+++ src/Footer.js
@@ -33,10 +33,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            {/* <!-- Copyright Section--> */}
-        <div className="copyright py-4 text-center text-white">
-            <div className="container"><small>Copyright &copy; Your Website 2022</small></div>
-        </div>
+            
         </footer>
 )
 
